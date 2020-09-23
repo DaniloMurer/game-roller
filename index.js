99d647b6ec6f878a07280cf79dc958108ef37d55
Danilo Jakob <danilo.murer@gmail.com>
2020-09-23 08:37:24 +0200

    CHANGE: User is able to join a game now

---
+++ index.js
@@ -0,0 +1,38 @@
+const config = require('./config.json');
+const Discord = require('discord.js');
+
+const client = new Discord.Client();
+
+
+client.on('message', (message) => {
+    // Check if the command has the from the bot neccessary prefix
+    if (message.content.startsWith(config.commandPrefix)) {
+        
+        const strippedCommand = message.content.slice(config.commandPrefix.length);
+        console.log(strippedCommand);
+        
+        // Check if the user wants to join a game
+        if (strippedCommand.split(' ')[1] === 'add') {
+            // Get the game name
+            const gameName = strippedCommand.split(' ')[2].toLowerCase();
+            console.log(gameName);
+            const user = message.guild.member(message.author);
+            
+            const role = message.guild.roles.cache.find( role => role.name === gameName);
+            if (role) {
+                user.roles.add(role);
+                message.reply('You were added to the game, you can now use the respective game channels :)');
+            } else {
+                message.reply('This game is not supported by the server yet!');
+            }
+        }
+    }
+});
+
+
+
+
+
+
+client.login(config.clientCode);
+
